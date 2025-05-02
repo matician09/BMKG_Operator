62f9bc4e90bf76db2298d6c0e36e401416c74234
Hugo Choe <hchoe@lenovo.com>
2025-05-02 14:13:42 +0700

    Add: Hello_OMP.f90

---
+++ 250502_ParallelProgramming/OpenMP/Hello_OMP.f90
@@ -0,0 +1,17 @@
+program hello
+
+        integer nthreads, tid, omp_get_num_threads
+        integer omp_get_thread_num
+
+        !$OMP PARALLEL PRIVATE(tid)
+
+        tid = omp_get_thread_num()
+        print *, "Hello World from thread = ", tid
+
+        if (tid .eq. 0) then
+                nthreads = omp_get_num_threads()
+                print *, "THREADS NUMBER = ", nthreads
+        endif
+        !$OMP END PARALLEL
+
+endprogram hello
